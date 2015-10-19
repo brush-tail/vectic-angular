7461bb13b14a448aad5509b50948d8a2b41dd3eb
Evan Lomas <evan@mylocalbroker.com.au>
2015-10-20 10:56:27 +1100

    Updated to include a directive and service for different implementation methods

--- index.js
+++ index.js
@@ -5,15 +5,35 @@ try { angular.module('vectic') } catch(err) {
 }
 
 angular.module('vectic')
-.factory('vectic', function vecticProvider() {
+.factory('vectic', function() {
   var _vectic = null;
 
     if (typeof vectic != 'undefined') {
       _vectic = vectic;
     }
     else {
-      console.error('vectic-angular could not find vectic');
+      if(!vectic) { return console.error('vectic-angular factory could not find vectic library dependancy'); }
     }
 
     return _vectic;
+})
+.directive('vectic', function() {
+  return {
+    template: '<div></div>',
+    restrict: 'E',
+    scope: {
+      id: '=',
+    },
+    link: function($scope, $element) {
+      // Check for vectic library
+      if(!vectic) { return console.error('vectic-angular directive could not find vectic library dependancy'); }
+
+      // Initialize if not done already
+      $scope.vectic = new vectic({
+        vecticID: $scope.id,
+        target: $($element),
+      });
+      $scope.vectic.init();
+    },
+  };
 });
